4f1fe3120661f33e4e5b79ce3dbf7b31f953bc24
makrem2 <makremjebali9@gmail.com>
2025-09-03 12:12:01 +0100

    03&09&25

--- server.js
+++ server.js
@@ -21,6 +21,16 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
+const db = require('./app/models');
+
+db.sequelize.sync();
+
+
+app.get("/",(req,res)=>{
+  res.json({message:'Welcome to Our Ecommerce Application'});
+})
+
+
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
